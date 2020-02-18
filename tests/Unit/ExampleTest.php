d42c89ade517ee0124c705dd2b4dee0633df180e
João Cruz <joao.fs.cruz97@gmail.com>
2020-02-18 19:45:55 +0000

    Setup testing

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest(): void
+    {
+        $this->assertTrue(true);
+    }
+
+}
