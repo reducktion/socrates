90c71796efdbd2368a03c749c3a3a441e0679628
Alexandre Almeida Olival <lex.olival@gmail.com>
2020-02-23 20:12:45 +0000

    Update DenmarkTest.php

--- tests/Feature/DenmarkTest.php
+++ tests/Feature/DenmarkTest.php
@@ -9,42 +9,49 @@ use Reducktion\Socrates\Facades\Socrates;
 
 class DenmarkTest extends FeatureTest
 {
-    public function test_extract_behaviour(): void
+    private $people;
+
+    protected function setUp(): void
     {
-        $people = [
+        parent::setUp();
+
+        $this->people = [
             'julius' => [
                 'cpr' => '090792-1395',
                 'gender' => Gender::MALE,
                 'dob' => Carbon::createFromFormat('Y-m-d', '1992-07-09'),
-                'age' => 27,
+                'age' => Carbon::createFromFormat('Y-m-d', '1992-07-09')->age,
             ],
             'naja' => [
                 'cpr' => '070593-0600',
                 'gender' => Gender::FEMALE,
                 'dob' => Carbon::createFromFormat('Y-m-d', '1993-05-07'),
-                'age' => 26,
+                'age' => Carbon::createFromFormat('Y-m-d', '1993-05-07')->age,
             ],
             'rolla' => [
                 'cpr' => '150437-3068',
                 'gender' => Gender::FEMALE,
                 'dob' => Carbon::createFromFormat('Y-m-d', '1937-04-15'),
-                'age' => 82,
+                'age' => Carbon::createFromFormat('Y-m-d', '1937-04-15')->age,
             ],
             'thomas' => [
                 'cpr' => '160888-1995',
                 'gender' => Gender::MALE,
                 'dob' => Carbon::createFromFormat('Y-m-d', '1988-08-16'),
-                'age' => 31,
+                'age' => Carbon::createFromFormat('Y-m-d', '1988-08-16')->age,
             ],
             'mia' => [
                 'cpr' => '040404-7094',
                 'gender' => Gender::FEMALE,
                 'dob' => Carbon::createFromFormat('Y-m-d', '2004-04-04'),
-                'age' => 15,
+                'age' => Carbon::createFromFormat('Y-m-d', '2004-04-04')->age,
             ],
         ];
+    }
 
-        foreach ($people as $person) {
+    public function test_extract_behaviour(): void
+    {
+        foreach ($this->people as $person) {
             $citizen = Socrates::getCitizenDataFromId($person['cpr'], 'DK');
 
             $this->assertEquals($person['gender'], $citizen->getGender());
@@ -55,14 +62,20 @@ class DenmarkTest extends FeatureTest
         $this->expectException(InvalidCprLengthException::class);
 
         Socrates::getCitizenDataFromId('324432-343', 'DK');
-
-        $this->expectException(InvalidCprLengthException::class);
     }
 
     public function test_validation_behaviour(): void
     {
-        $this->assertTrue(
-            Socrates::validateId('251195-1448', 'DK')
-        );
+        foreach ($this->people as $person) {
+            $this->assertTrue(
+                Socrates::validateId($person['cpr'], 'DK')
+            );
+        }
+
+        foreach (['234321-2454', '333694-0034', '088383-2313', '133232-1323', '040404-7054'] as $cpr) {
+            $this->assertFalse(
+                Socrates::validateId($cpr, 'DK')
+            );
+        }
     }
 }
