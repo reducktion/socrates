7861a5f7d8c58114728eecdc54e418f09f5ca2cd
João Cruz <joao.fs.cruz97@gmail.com>
2020-04-10 21:48:00 +0000

    WIP pob extraction

---
+++ src/Core/France/FranceIdValidator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Reducktion\Socrates\Core\France;
+
+use Reducktion\Socrates\Contracts\IdValidator;
+use Reducktion\Socrates\Exceptions\InvalidLengthException;
+
+class FranceIdValidator implements IdValidator
+{
+    public function validate(string $id): bool
+    {
+        $id = $this->sanitize($id);
+
+        $checksum = (int) substr($id, -2);
+        $id = substr($id, 0, -2);
+
+        $result = 97 - ((int) $id % 97);
+
+        return $checksum === $result;
+    }
+
+    private function sanitize(string $id): string
+    {
+        $id = str_replace(' ', '', $id);
+
+        $idLength = strlen($id);
+
+        if ($idLength !== 15) {
+            throw new InvalidLengthException("The French NIR must have 15 digits, got $idLength");
+        }
+
+        return $id;
+    }
+}
---
+++ tests/Feature/FranceTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Reducktion\Socrates\Tests\Feature;
+
+use Carbon\Carbon;
+use Reducktion\Socrates\Constants\Gender;
+use Reducktion\Socrates\Exceptions\InvalidLengthException;
+use Reducktion\Socrates\Facades\Socrates;
+
+class FranceTest extends FeatureTest
+{
+    private $people;
+    private $invalidIds;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->people = [
+            'Annette' => [
+                'nir' => '2820819398814 09',
+                'gender' => Gender::FEMALE,
+                'dob' => Carbon::createFromFormat('Y-m', '1982-08'),
+                'age' => Carbon::createFromFormat('Y-m', '1982-08')->age,
+                'pob' => 'Corrèze'
+            ],
+            'Lance' => [
+                'nir' => '1350455179061 16',
+                'gender' => Gender::MALE,
+                'dob' => Carbon::createFromFormat('Y-m', '1935-04'),
+                'age' => Carbon::createFromFormat('Y-m', '1935-04')->age,
+                'pob' => 'Meuse'
+            ],
+            'Ancelote' => [
+                'nir' => '2381080214568 11',
+                'gender' => Gender::FEMALE,
+                'dob' => Carbon::createFromFormat('Y-m', '1938-10'),
+                'age' => Carbon::createFromFormat('Y-m', '1938-10')->age,
+                'pob' => 'Somme'
+            ],
+            'Lothair' => [
+                'nir' => '1880858704571 57',
+                'gender' => Gender::MALE,
+                'dob' => Carbon::createFromFormat('Y-m', '1988-08'),
+                'age' => Carbon::createFromFormat('Y-m', '1988-08')->age,
+                'pob' => 'Nièvre'
+            ],
+            'Millard' => [
+                'nir' => '1030307795669 72',
+                'gender' => Gender::MALE,
+                'dob' => Carbon::createFromFormat('Y-m', '2003-03'),
+                'age' => Carbon::createFromFormat('Y-m', '2003-03')->age,
+                'pob' => 'Ardèche'
+            ]
+        ];
+
+        $this->invalidIds = [
+            '1031629895669 72',
+            '2312763214568 54',
+            '1031622192811 22',
+            '2312763291021 11',
+            '2312760989812 01'
+        ];
+    }
+
+    public function test_extract_behaviour(): void
+    {
+        foreach ($this->people as $person) {
+            $citizen = Socrates::getCitizenDataFromId($person['nir'], 'FR');
+            $this->assertEquals($person['gender'], $citizen->getGender());
+            $this->assertEquals($person['dob'], $citizen->getDateOfBirth());
+            $this->assertEquals($person['age'], $citizen->getAge());
+            $this->assertEquals($person['pob'], $citizen->getPlaceOfBirth());
+        }
+
+        $this->expectException(InvalidLengthException::class);
+
+        Socrates::getCitizenDataFromId('10316221921 22', 'FR');
+    }
+
+    public function test_validation_behaviour(): void
+    {
+        foreach ($this->people as $person) {
+            $this->assertTrue(
+                Socrates::validateId($person['nir'], 'FR')
+            );
+        }
+
+        foreach ($this->invalidIds as $nir) {
+            $this->assertFalse(
+                Socrates::validateId($nir, 'FR')
+            );
+        }
+
+        $this->expectException(InvalidLengthException::class);
+
+        Socrates::validateId('23127609898 01', 'FR');
+    }
+
+}
