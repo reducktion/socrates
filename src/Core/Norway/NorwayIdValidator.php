6f6d782a6196740f3d0290fd69e8cbc129df16bc
Alexandre Olival <alexandre.olival@compareeuropegroup.com>
2020-04-14 20:35:48 +0100

    Implement validator

--- src/Core/Norway/NorwayIdValidator.php
+++ src/Core/Norway/NorwayIdValidator.php
@@ -16,8 +16,35 @@ class NorwayIdValidator implements IdValidator
             throw new InvalidLengthException("Norwegian fødselsnummer must have 11 digits, got $idLength");
         }
 
+        $firstControlDigit = (int) $id[9];
+        $secondControlDigit = (int) $id[10];
 
+        $firstDayDigit = (int) $id[0];
+        $secondDayDigit = (int) $id[1];
+        $firstMonthDigit = (int) $id[2];
+        $secondMonthDigit = (int) $id[3];
+        $firstYearDigit = (int) $id[4];
+        $secondYearDigit = (int) $id[5];
+        $firstPersonalDigit = (int) $id[6];
+        $secondPersonalDigit = (int) $id[7];
+        $thirdPersonalDigit = (int) $id[8];
 
-        return true;
+        $firstTest = 11 - (((3 * $firstDayDigit) + (7 * $secondDayDigit)
+                    + (6 * $firstMonthDigit) + $secondMonthDigit
+                    + (8 * $firstYearDigit) + (9 * $secondYearDigit)
+                    + (4 * $firstPersonalDigit) + (5 * $secondPersonalDigit) + (2 * $thirdPersonalDigit)) % 11);
+
+        $firstControlResult = $firstTest === 11 ? 0 : $firstTest;
+
+        $secondTest = 11 - (((5 * $firstDayDigit) + (4 * $secondDayDigit)
+                    + (3 * $firstMonthDigit) + (2 * $secondMonthDigit)
+                    + (7 * $firstYearDigit) + (6 * $secondYearDigit)
+                    + (5 * $firstPersonalDigit) + (4 * $secondPersonalDigit) + (3 * $thirdPersonalDigit) + (2 * $firstControlResult)) % 11);
+
+        $secondControlResult = $secondTest === 11 ? 0 : $secondTest;
+
+        return $firstControlDigit === $firstControlResult
+            && $secondControlDigit === $secondControlResult;
     }
+
 }
