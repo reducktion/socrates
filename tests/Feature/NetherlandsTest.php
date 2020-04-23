003858730e82e35d2f7b2a27ade83e5ad0b018ba
João Cruz <joao.fs.cruz97@gmail.com>
2020-04-23 18:48:28 +0000

    Implement Netherlands

---
+++ tests/Feature/NetherlandsTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Reducktion\Socrates\Tests\Feature;
+
+use Reducktion\Socrates\Exceptions\InvalidLengthException;
+use Reducktion\Socrates\Exceptions\UnsupportedOperationException;
+use Reducktion\Socrates\Facades\Socrates;
+
+class NetherlandsTest extends FeatureTest
+{
+    private $validIds;
+    private $invalidIds;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->validIds = [
+            '271138488',
+            '193006248',
+            '398061300',
+            '719102091',
+            '692676491',
+        ];
+
+        $this->invalidIds = [
+            '692676411',
+            '918271871',
+            '998786123',
+            '123128764',
+            '817187288',
+        ];
+    }
+
+    public function test_extract_behaviour(): void
+    {
+        $this->expectException(UnsupportedOperationException::class);
+
+        Socrates::getCitizenDataFromId('', 'NL');
+    }
+
+    public function test_validation_behaviour(): void
+    {
+        foreach ($this->validIds as $id) {
+            $this->assertTrue(
+                Socrates::validateId($id, 'NL')
+            );
+        }
+
+        $this->expectException(InvalidLengthException::class);
+
+        Socrates::validateId('692676', 'NL');
+
+
+        foreach ($this->invalidIds as $id) {
+            $this->assertFalse(
+                Socrates::validateId($id, 'NL')
+            );
+        }
+    }
+
+}
